3d687880b0faf19ef4471393bc1b9361904e1188
dan-e3125 <97024719+dan-e3125@users.noreply.github.com>
2024-02-18 13:06:34 +0200

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,5 @@
     region = "us-east-1"
-    access_key = "AKIAUNWS2JVLWKIWASH4"
-    secret_key = "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"
 }
-
 
 resource "aws_instance" "myec2" {
     ami = "ami-0277155c3f0ab2930"
